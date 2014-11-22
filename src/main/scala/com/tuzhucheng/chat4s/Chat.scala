f9d8ece8379f41f645f3c928bcad0e143bc3a086
Michael Tu <tuzhucheng@outlook.com>
2014-11-21 22:15:01 -0800

    make port read from environment variable

--- src/main/scala/com/tuzhucheng/chat4s/Chat.scala
+++ src/main/scala/com/tuzhucheng/chat4s/Chat.scala
@@ -1,9 +1,12 @@
 package com.tuzhucheng.chat4s
 
+import util.Properties
 import org.http4s.server.jetty.JettyBuilder
 
 object Chat extends App {
-  JettyBuilder.bindHttp(8080)
+  val port = Properties.envOrElse("PORT", "8080").toInt
+  println("Starting on port " + port)
+  JettyBuilder.bindHttp(port)
     .mountService(ChatService.service, "/chat4s")
     .run
     .awaitShutdown()
